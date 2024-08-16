0e7df90b10859897608e7eae8bfb2b00e1341932
Stephen <ding.q.stephen@gmail.com>
2024-08-16 15:48:45 -0400

    calculator only works with single digit numbers. changing array logic.

--- script.js
+++ script.js
@@ -13,13 +13,12 @@ numberButtons.forEach((button) =>{
         const value = button.innerText;
         console.log(value);
         if(storage[0]==undefined){
-            firstNum = value;
             storage.push(value);
             display.innerText = value;
         }
         else if (storage[1]!=undefined && storage[2]==undefined){
-            secondNum = value;
             storage.push(value);
+            display.innerText = value;
         }
         else{
             // idk yet
@@ -30,6 +29,7 @@ numberButtons.forEach((button) =>{
 
 functionButtons.forEach((button) =>{
     button.addEventListener("click", () =>{
+        pressFunction = true;
         console.log(button.id);
         const operation = button.id;
         if(storage[0]!= undefined && storage[1] == undefined){
@@ -39,24 +39,45 @@ functionButtons.forEach((button) =>{
             && storage[1]!= undefined
             && storage[2]!= undefined
         ){
-        switch(operation){
-            case "delete":
-                storage[0] = undefined; //first # cleared
-                storage[2] = undefined; //second # cleared
-            case "add":
-                add(storage[0], storage[2]);
-            case "subtract":
-                subtract(storage[0], storage[2]);
-            case "multiply":
-                multiply(storage[0], storage[2]);
-            case "divide":
-                divide(storage[0], storage[2]);
-        } 
+        const num2 = storage.pop();
+        const operand = storage.pop();
+        const num1 = storage.pop();
+        storage.push(operate(num1, operand, num2));
+        display.innerText = storage[0];
+        pressFunction = false;
+        // need to then store operate result as first index storage[0]
         }
+        else if(storage[0]!= undefined
+            && storage[1]!= undefined
+            && storage[2]== undefined){ //replacing the operand if they change their mind mid-way
+                storage.pop();
+                storage.push(operation);
+            }
     });
     });
 
-function operate(num1,operand, num2){
+function operate(firstNum, operand, secondNum){
+    const num1 = parseInt(firstNum);
+    const num2 = parseInt(secondNum);
+
+    switch(operand){
+        case "delete":
+                storage.pop();        
+        case "add":
+            return add(num1, num2);
+        case "subtract":
+            return subtract(num1, num2);
+        case "multiply":
+            return multiply(num1, num2);
+        case "divide":
+            return divide(num1, num2);
+        case "all-clear":
+            while (storage.length > 0) {
+                storage.pop();
+              }      
+            display.innerText = "";
+        case ".": // what happens if they press the decimal button mid-calculation?
+    } 
 
 }
 
@@ -73,5 +94,8 @@ function multiply(num1, num2){
 }
 
 function divide(num1, num2){
-    return num1/num2;
+    if(num2!=0) 
+    return num1/num2; 
+    else
+    return "ERROR"; // might change later
 }
