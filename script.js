618debca2cff07dd740607cf2c7ee7d67cdb99d6
Stephen <ding.q.stephen@gmail.com>
2024-08-20 23:15:31 -0400

    basic arithemtic works. revised logic. removed operate() function and embedded it in the event listener

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-let firstNum, secondNum, operand;
+let firstNum, secondNum, previousOperand, currentOperand;
 
 let digits =[];
 
 let stepResults = [];
 
 let displayValue = "";
+
 
 const numberButtons = document.querySelectorAll(".number");
 const functionButtons = document.querySelectorAll(".function");
@@ -27,6 +28,102 @@ numberButtons.forEach((button) =>{
 });
 
 functionButtons.forEach((button) =>{
+    button.addEventListener("click", () =>{
+        convertToNumber();
+        if(previousOperand == undefined){
+            previousOperand = button.id;
+        }
+        else
+            currentOperand = button.id;
+        console.log(`previous operand: ${previousOperand}`);
+        console.log(`current operand: ${currentOperand}`);
+
+        if(button.id == "all-clear"){
+            clearDisplay();
+        }
+        else if(button.id == "delete"){
+                console.log("deletion");
+                digits.pop();
+                console.log(`this is digits[]:`);
+                console.log(digits);
+                let temp = String(displayValue).slice(0, displayValue.length-1);
+                displayValue = temp;
+                display.innerText = displayValue;
+        }
+        else if (stepResults.length>=2){
+            const num2 = stepResults.pop();
+            const num1 = stepResults.pop();
+            let result;
+            switch(previousOperand){
+                case("add"):
+                    console.log("addition");
+                    displayValue = String(add(num1, num2));
+                    result = add(num1, num2);
+                    break;
+                case("subtract"):
+                    console.log("subtraction");
+                    displayValue = String(subtract(num1, num2));
+                    result = subtract(num1, num2);
+                    break;
+                case("multiply"):
+                    console.log("multiplication");
+                    displayValue = String(multiply(num1, num2));
+                    result = multiply(num1, num2);
+                    break;
+                case("divide"):
+                    console.log("division");
+                    displayValue = String(divide(num1, num2));
+                    result = divide(num1, num2);
+                    break;
+                case("decimal"):
+                    console.log("decimaling");
+                    displayValue+=".";
+                    digits.push(".");
+                    break;
+            }
+            if(typeof(result)== "number" && result != NaN){
+                stepResults.push(result);
+                console.log(`the result should be: ${result}`);
+            }
+            display.innerText = displayValue;
+        }
+        displayValue = "";
+        if (currentOperand == "equals"|| button.id == "delete" || button.id =="all-clear"){
+            previousOperand = undefined;
+            currentOperand = undefined;
+        }
+       /*  if (currentOperand != "equals" && currentOperand != "all-clear") {
+            previousOperand = currentOperand;
+        } */
+    });
+    button.addEventListener("mousedown", () =>{
+        button.setAttribute("style", "background-color: rgba(141, 102, 232,0.2)");
+    })
+    button.addEventListener("mouseup", ()=>{
+        button.setAttribute("style", "background-color: rgb(164, 131, 242)");
+    })
+});
+
+function convertToNumber (){
+    let numString = "";
+    while (digits.length >0){
+        numString+= digits.shift();
+    }
+    console.log("convertToNumber executing");
+    let temp;
+    if(numString.includes("."))
+        temp = parseFloat(numString);
+    else 
+        temp = parseInt(numString);
+    console.log(temp);
+    if(numString!=""){
+        stepResults.push(temp);
+        console.log(stepResults);
+    }
+}
+
+
+/* functionButtons.forEach((button) =>{
     button.addEventListener("click", () =>{
         convertToNumber(button.id); //this way the second number gets stored first
         if(button.id == "all-clear"){
@@ -101,7 +198,7 @@ function operate(num1, operation, num2){
             displayValue+=".";
         }
 
-    }
+    } */
 
 function clearDisplay(){
     displayValue = "";
